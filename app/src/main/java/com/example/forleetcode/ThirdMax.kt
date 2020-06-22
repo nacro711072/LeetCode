4c1ecf9dd2644ef86a08e203e5fb6147446fd263
nick <nickyang@twdd.com.tw>
2020-06-22 17:07:36 +0800

    # Update -> [414] Third Max.

---
+++ app/src/main/java/com/example/forleetcode/ThirdMax.kt
@@ -0,0 +1,57 @@
+package com.example.forleetcode
+
+import java.util.*
+
+//Given a non-empty array of integers, return the third maximum number in this a
+//rray. If it does not exist, return the maximum number. The time complexity must
+//be in O(n).
+//
+// Example 1:
+//
+//Input: [3, 2, 1]
+//
+//Output: 1
+//
+//Explanation: The third maximum is 1.
+//
+//
+//
+// Example 2:
+//
+//Input: [1, 2]
+//
+//Output: 2
+//
+//Explanation: The third maximum does not exist, so the maximum (2) is returned
+//instead.
+//
+//
+//
+// Example 3:
+//
+//Input: [2, 2, 3, 1]
+//
+//Output: 1
+//
+//Explanation: Note that the third maximum here means the third maximum distinct
+// number.
+//Both numbers with value 2 are both considered as second maximum.
+//
+// Related Topics Array
+
+/**
+ * Nick, 2020/6/22
+ * [414] Third Maximum Number
+ */
+class ThirdMax {
+    fun thirdMax(nums: IntArray): Int {
+        val top = TreeSet<Int>()
+        for (n in nums) {
+            top.add(n)
+            if (top.size > 3) {
+                top.pollFirst()
+            }
+        }
+        return if (top.size < 3) top.last() else top.first()
+    }
+}
