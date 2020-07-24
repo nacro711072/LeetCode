6b40b09392d6b26406ebd996a8d27efc072769b9
nick <nacro711072@yahoo.com.tw>
2020-07-25 00:09:25 +0800

    # Update -> [53].

--- app/src/main/java/com/example/forleetcode/array/MaximumSubArray53.kt
+++ app/src/main/java/com/example/forleetcode/array/MaximumSubArray53.kt
@@ -1,4 +1,15 @@
 package com.example.forleetcode.array
 
-class MaximumSubarray53 {
+class MaximumSubArray53 {
+    fun maxSubArray(nums: IntArray): Int {
+        var acc = nums[0]
+        var max: Int = nums[0]
+        for (i in 1..nums.lastIndex) {
+            val temp = Math.max(acc, nums[i - 1]) + nums[i]
+            acc = Math.max(temp, nums[i])
+            max = Math.max(max, acc)
+        }
+        return max
+    }
+
 }
