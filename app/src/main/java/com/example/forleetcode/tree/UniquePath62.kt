93d54b056ebd8232fcf6d7b5deb55a738d2a14c7
nick <nacro711072@yahoo.com.tw>
2020-07-25 17:37:13 +0800

    # Update -> [62] UniquePath

---
+++ app/src/main/java/com/example/forleetcode/tree/UniquePath62.kt
@@ -0,0 +1,14 @@
+package com.example.forleetcode.tree
+
+class UniquePath62 {
+    fun uniquePaths(m: Int, n: Int): Int {
+        val dp: Array<IntArray> = Array(m) { IntArray(n) { 1 } }
+        for (i in 1 until m) {
+            for (j in 1 until n) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+            }
+        }
+
+        return dp[m - 1][n - 1]
+    }
+}
